589947ddfe031505c945893957d6e79ec101c77a
mgmetehan <mgmetehan@hotmail.com>
2024-08-30 19:57:51 +0300

    init project

---
+++ src/main/java/com/mgmetehan/user_jwt_security_example/UserJwtSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.user_jwt_security_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJwtSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserJwtSecurityExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/user_jwt_security_example/UserJwtSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.user_jwt_security_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJwtSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
